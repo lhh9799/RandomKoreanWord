7aad4bbb8bac570bcb0111550c38a59501951844
Hyunho Lee <lhh9799@gmail.com>
2023-07-26 22:26:03 +0900

    test

--- src/com/ssafy/view/RandomWordTest.java
+++ src/com/ssafy/view/RandomWordTest.java
@@ -2,6 +2,6 @@ package com.ssafy.view;
 
 public class RandomWordTest {
     public static void main(String[] args) {
-        System.out.println("PM 최강센터 갓 이현호");
+        System.out.println("춤신춤왕 김내림");
     }
 }
